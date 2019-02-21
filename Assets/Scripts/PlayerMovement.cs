93a06c9023904990711fb2947c893a9e5ce66cab
Shappy <HapShapIncorporated@gmail.com>
2019-02-21 15:04:36 -0500

    More Networked Players

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate(){
+        transform.Rotate(0, 0, -Input.GetAxisRaw("Horizontal") * 100f * Time.deltaTime);
+
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            Network.Move(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rb2d.AddForce(transform.up * 5f * Input.GetAxisRaw("Vertical"));
+            //Network.Move(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"));
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovementNetwork.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementNetwork : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    public float v, h;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate(){
+        transform.Rotate(0, 0, -h * 100f * Time.deltaTime);
+
+        if(v > 0 || h != 0)
+        {
+            Network.Move(v, h);
+        }
+
+        if (v > 0)
+        {
+            rb2d.AddForce(transform.up * 5f * v);
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+    }
+}
